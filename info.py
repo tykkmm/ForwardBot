8599a18c964030a716b433808751b8322010d30c
tykkmm <tykhanew45@gmail.com>
2024-07-23 06:49:22 +0500

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '1455678'))
-API_HASH = environ.get('API_HASH', 'a6b02f54dab5607e542eb216f28a1e6d')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6635160184:AAFp4FhPN5Lkk8BIC8kLuZu9smqkDka57tQ')
+API_ID = int(environ.get('API_ID', '29426486'))
+API_HASH = environ.get('API_HASH', d71ad4ec048ab41677a1a439b21ff0c9')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '6769157231'))
+OWNER = int(environ.get('OWNER', '5976437467'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
-UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', 'https://t.me/webverseupdates')
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/webversegroup')
-DEV_LINK = environ.get('DEV_LINK', 'https://t.me/bhaiyajixbot')
+UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', 'https://t.me/+MaoyCcGTqLRmMjM0')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/+MaoyCcGTqLRmMjM0')
+DEV_LINK = environ.get('DEV_LINK', 'http://t.me/TY3052432')
 
 # customise as per your need
